b56077abb5cf509a0f3bf789f589c05652b22e75
PradanaAkbarRazan <pradana.akbar.razan@gmail.com>
2026-03-03 14:17:49 +0700

    Modul2pretest

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("===============================");
+         System.out.println("KARTU TANDA MAHASISWA");
+          System.out.println("Nama : Pradana Akbar Razan");
+           System.out.println("NIM : 105224005");
+            System.out.println("Prodi : Ilmu Komputer");
+             System.out.println("===============================");
+    }
+}
